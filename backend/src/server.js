bb80fa7d6fc06a4de21a9ae07bac3d1cc7e49a69
krishnadev7 <krishnadevv12@gmail.com>
2025-01-31 18:32:31 +0530

    added settings for render deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,9 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectMongoDb } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,14 @@ const PORT = process.env.PORT || 8000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html "));
+  });
+}
 
 server.listen(PORT || 8000, () => {
   console.log(`Server started on port ${PORT}`);
